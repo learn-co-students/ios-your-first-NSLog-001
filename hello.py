8b507e08a6325bce454f596fd461fbef9d5647b9
Craig Calderone <craig@chartbeat.com>
2016-05-06 13:51:10 -0400

    update python version and remove port 8888

--- hello.py
+++ hello.py
@@ -95,5 +95,4 @@ def max_concurrents(apikey, domain, start, end, save_to=False):
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
